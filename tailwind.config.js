fb1ba6a7676dd3c556a5c8af9dd1a5e7f69385e3
Manan Agrawal <agrawalmanan2@gmail.com>
2024-10-08 00:00:11 +0530

    initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,17 @@
-   // tailwind.config.js
-   module.exports = {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
